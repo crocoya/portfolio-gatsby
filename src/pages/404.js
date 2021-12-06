cbf223acb654f2bc2d2cd2cf537f97a415b97b9e
Crocoya <yassine.tababi@outlook.com>
2021-12-07 00:02:46 +0100

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-// markup
 const NotFoundPage = () => {
   return (
-    <main className='main--notfound'>
+    <section className='main--notfound'>
       <title>Not found</title>
       <div className='content--notfound'>
         <h1>404</h1>
@@ -13,7 +12,7 @@ const NotFoundPage = () => {
         </div>
       </div>
       <Link to='/'>Go home</Link>
-    </main>
+    </section>
   );
 };
 
